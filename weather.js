b9b6fba937e3f023d7427d6d6ee973df4cf944f6
unknown <alinsabu98@gmail.com>
2021-12-09 12:46:11 +0530

    upload

--- weather.js
+++ weather.js
@@ -27,8 +27,11 @@
 
 var inputValue = document.querySelector('.inputValue');
 var name = document.querySelector('.name');
+var date = document.querySelector('.date');
+var country = document.querySelector('.country');
+var feels_like = document.querySelector('.feels_like');
 var temp = document.querySelector('.temp');
-
+var temp_max = document.querySelector('.temp_max');
 var desc = document.querySelector('.desc');
 //var clouds = document.querySelector('.clouds');
 var button= document.querySelector('.button');
@@ -39,13 +42,23 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&ap
 .then(response => response.json())
 .then(data => {
   var tempValue = data['main']['temp_min'];
-  
+  var temp_maxValue = data['main']['temp_max'];
   var nameValue = data['name'];
   var descValue = data['weather'][0]['description'];
+  var feels_likeValue = data['main']['feels_like'];
+  var countryValue = data['sys']['country'];
+  var dateValue = data['dt'];
 
+  var day = new Date(dateValue*1000);
+
+  country.innerHTML = countryValue;
   name.innerHTML = nameValue;
   desc.innerHTML = descValue;
-  temp.innerHTML = tempValue+' '+ ' oC';
+  temp.innerHTML = tempValue+' '+ ' o'.sup()+'C'+' / '+temp_maxValue+' '+ ' o'.sup()+'C';
+  feels_like.innerHTML = feels_likeValue+' '+' o'.sup()+'C';
+  date.innerHTML = day.toString();
+
+  // temp_max.innerHTML = temp_maxValue+' '+ ' o'.sup()+'C';
 
   //input.value ="";
 
